f79c2c5e602ea9d56932c80360b8d9c23fb93275
Jet Holloway <pwnworthy@gmail.com>
2015-02-05 23:26:09 +1100

    Learnt how to deal with multiple faders

--- arduino_midi_test.cpp
+++ arduino_midi_test.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include "/home/jet/tmp/arduino_midi_test/MIDI/MIDI.h"
 
-void do_fader();
+void do_fader( unsigned int fader_i );
 
 int main(void)
 {
@@ -30,12 +30,15 @@ MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MySettings);
 
 // Pins
 const int ledPin = 13;          // Select the pin for the LED
-const int fader_analog_pin  = 0;           // Position of fader relative to GND (Analog 0)
 
 // Variables
 const int fader_max          = 1023;  // Value read by fader's maximum position (0-1023)
 const int fader_min          = 0;     // Value read by fader's minimum position (0-1023)
-const int fader_midi_channel = 1;     // Value from 1-16
+
+// Descriptions of Faders
+const unsigned int nr_faders = 2;
+const int fader_analog_pin[]   = {0,1};
+const int fader_midi_channel[] = {1,2};
 
 void setup()
 {
@@ -51,7 +54,8 @@ void setup()
 void loop()
 {
     // Read the value from the sensor:
-    do_fader();
+    for ( unsigned int i = 0; i < nr_faders; i++ )
+        do_fader(i);
 
     // Turn the ledPin on for vel>>6 milliseconds
     digitalWrite(ledPin, HIGH);
@@ -62,12 +66,10 @@ void loop()
     delay(250);
 }
 
-// Returns a MIDI pitch bend value for the fader's current position
-// Cases ensure that there is a -infinity (min) and max value despite possible math error
-void do_fader()
+void do_fader( unsigned int fader_i )
 {
     // Analog input position, and corresponding MIDI position
-    int analog_pos = analogRead(fader_analog_pin);
+    int analog_pos = analogRead(fader_analog_pin[fader_i]);
     int midi_pos = 0;
 
     if (analog_pos <= fader_min)
@@ -77,5 +79,5 @@ void do_fader()
     else
         midi_pos = ((long int)(analog_pos-fader_min)*16383) / (fader_max-fader_min);
 
-    MIDI.sendPitchBend(midi_pos - 8192, fader_midi_channel);
+    MIDI.sendPitchBend(midi_pos - 8192, fader_midi_channel[fader_i]);
 }
