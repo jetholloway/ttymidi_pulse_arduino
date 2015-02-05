0ada46a36040564cb053b098c4d7c1d4199236f0
Jet Holloway <pwnworthy@gmail.com>
2015-02-06 00:40:10 +1100

    Only send MIDI values if they have changed sufficiently.
    This stops it spamming the MIDI serial bus.

--- arduino_midi_test.cpp
+++ arduino_midi_test.cpp
@@ -36,9 +36,11 @@ const int fader_max          = 1023;  // Value read by fader's maximum position 
 const int fader_min          = 0;     // Value read by fader's minimum position (0-1023)
 
 // Descriptions of Faders
+const int fader_difference = 1;         // Fader value must change by *more* than this
 const unsigned int nr_faders = 2;
 const int fader_analog_pin[]   = {0,1};
 const int fader_midi_channel[] = {1,2};
+int fader_prev[]               = {0,0};
 
 void setup()
 {
@@ -79,12 +81,27 @@ void do_fader( unsigned int fader_i )
     int analog_pos = analogRead(fader_analog_pin[fader_i]);
     int midi_pos = 0;
 
-    if (analog_pos <= fader_min)
-        midi_pos = 0;
-    else if (analog_pos >= fader_max)
-        midi_pos = 16383;
-    else
-        midi_pos = ((long int)(analog_pos-fader_min)*16383) / (fader_max-fader_min);
+    // Only bother with it if the value has changed sufficiently
+    if ( abs(analog_pos-fader_prev[fader_i]) > fader_difference )
+    {
+        // If the value changes loads, then just accept the new value
+        if ( abs(analog_pos-fader_prev[fader_i]) > 2*fader_difference )
+            fader_prev[fader_i] = analog_pos;
+        else // If the new value is just outside the range of accepted values, then just tweak the prev value
+        {
+            if ( analog_pos > fader_prev[fader_i] )
+                fader_prev[fader_i]++;
+            if ( analog_pos < fader_prev[fader_i] )
+                fader_prev[fader_i]--;
+        }
 
-    MIDI.sendPitchBend(midi_pos - 8192, fader_midi_channel[fader_i]);
+        if (analog_pos <= fader_min)
+            midi_pos = 0;
+        else if (analog_pos >= fader_max)
+            midi_pos = 16383;
+        else
+            midi_pos = ((long int)(analog_pos-fader_min)*16383) / (fader_max-fader_min);
+
+        MIDI.sendPitchBend(midi_pos - 8192, fader_midi_channel[fader_i]);
+    }
 }
