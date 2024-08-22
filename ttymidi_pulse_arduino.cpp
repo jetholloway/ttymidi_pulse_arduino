0b6a4e37bab28618a5adb3056aecb853a8c82f55
Jet Holloway <jet.holloway@gmail.com>
2024-08-22 21:27:14 +1000

    Average measurements over 16 samples to reduce noise

--- ttymidi_pulse_arduino.cpp
+++ ttymidi_pulse_arduino.cpp
@@ -28,11 +28,14 @@ struct MySettings : public midi::DefaultSettings
 MIDI_CREATE_CUSTOM_INSTANCE(HardwareSerial, Serial, MIDI, MySettings);
 
 // Constants
-const int fader_max = 1023; // Value read by fader's maximum position (0-1023)
-const int fader_min = 0;    // Value read by fader's minimum position (0-1023)
+// NOTE: `N_SAMPLES` must be 16, as implicitly we're multiplying the fader value by 16 to get the MIDI value (as the ADC has values in
+// 0-1023, but MIDI has values in 0-16385).
+#define N_SAMPLES 16
 
 // Descriptions of Faders
-const int fader_difference = 2; // Fader value must change by *more* than this
+// Fader value must change by *more* than this for us to send an update.  (This is after 'multiplying' by 16, i.e. the full range is
+// 0-16385)
+const int jenga_threshold = 32;
 const unsigned int nr_faders   = 3;
 const int fader_analog_pin[]   = {0,1,2};
 const int fader_midi_channel[] = {1,2,3};
@@ -92,33 +95,28 @@ void send_analogue_pos_over_MIDI( int MIDI_channel, int analog_pos )
 {
 	int midi_pos = 0;
 
-	if (analog_pos <= fader_min)
+	if (analog_pos <= 0)
 		midi_pos = 0;
-	else if (analog_pos >= fader_max)
+	else if (analog_pos >= 16383)
 		midi_pos = 16383;
 	else
-		midi_pos = ((long int)(analog_pos-fader_min)*16383) / (fader_max-fader_min);
+		midi_pos = analog_pos;
 
 	MIDI.sendPitchBend(midi_pos - 8192, MIDI_channel);
 }
 
 void do_fader( unsigned int fader_i )
 {
-	//   The Arduino only has one ADC shared between all channels.  This is an
-	// extra read to flush the old value out of the sample-and-hold-buffer (or
-	// however it works internally).  If you don't have this, the previously
-	// read analogue value will affect this one.
-	// I have tested it, and only one extra read is necessary
-	analogRead(fader_analog_pin[fader_i]);
-
-	// Analog input position, and corresponding MIDI position
-	int analog_pos = analogRead(fader_analog_pin[fader_i]);
+	// Read multiple times and sum to reduce noise
+	int analog_pos = 0;
+	for (int i = 0; i < N_SAMPLES; i++)
+		analog_pos += analogRead(fader_analog_pin[fader_i]);
 
 	// Only bother with it if the value has changed sufficiently
-	if ( abs(analog_pos-fader_prev[fader_i]) > fader_difference )
+	if ( abs(analog_pos-fader_prev[fader_i]) > jenga_threshold )
 	{
 		// If the value changes by a lot, then just accept the new value
-		if ( abs(analog_pos-fader_prev[fader_i]) > 2*fader_difference )
+		if ( abs(analog_pos-fader_prev[fader_i]) > 2*jenga_threshold )
 			fader_prev[fader_i] = analog_pos;
 		else // If the new value is just outside the range of accepted values, then just tweak the prev value
 		{
